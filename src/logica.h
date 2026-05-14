67266abea8dc550afeea487e99df6ac935af50ba
Crisboned <c.boned@alumnos.upm.es>
2026-05-14 18:28:53 +0200

    Clase lógica

---
+++ src/logica.h
@@ -0,0 +1,13 @@
+#pragma once
+class Tablero;
+class Logica
+{
+private:
+    Tablero* tablero;
+public:
+    Logica(Tablero* t);
+    void tecla(unsigned char key);
+    void teclaLiberada(unsigned char key);
+    void actualizarMovimiento();
+
+};
